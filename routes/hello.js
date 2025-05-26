9abbeedb90e712f7f0d4f4faf18396452773f916
LAPTOP-KB8BC5LL\koume <aikawaryosuke14@gmail.com>
2025-05-26 16:37:25 +0900

    Chapter4終了しました。

--- routes/hello.js
+++ routes/hello.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const http = require('http');
+const http = require('https');
 const parseString = require('xml2js').parseString;
 
 //get
-router.get('/', (req,res,vext) => {
+router.get('/', (req,res,next) => {
   var opt = {
     host: 'news.google.com',
     port: 443,
-    path: '/rss?hl=ja&ie=UTF-8&or=UTF-8&gl=jp&ceid=JP:ja'
+    path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja',
   };
-
-  http:get(opt, (res2) => {
+  http.get(opt, (res2) => {
     var body ='';
+    
     res2.on('data', (data) => {
+      body += data;
+    });
+
+    res2.on('end', () => {
       parseString(body.trim(), (err, result) => {
         console.log(result);
         var data = {
           title: 'Google News',
-          content: result.rss.channel[0].itm
+          content: result.rss.channel[0].item
         };
         res.render('hello', data);
       });
